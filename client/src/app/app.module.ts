9bf65a299d3f4b8e9519b1c8960cb7366bd74a15
nikolasnelson <nelson.nikolas@gmail.com>
2019-06-04 20:35:55 +0100

    adding footer, top recipes and some changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { HeroComponent } from './hero/hero.component';
+import { HeroComponent } from './home/hero/hero.component';
+import { TopRecipesComponent } from './home/top-recipes/top-recipes.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HeroComponent
+    HeroComponent,
+    TopRecipesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/home/top-recipes/top-recipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-recipes',
+  templateUrl: './top-recipes.component.html',
+  styleUrls: ['./top-recipes.component.scss']
+})
+export class TopRecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
